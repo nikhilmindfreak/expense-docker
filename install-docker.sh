c4b6ab6db8b4a71ef119f783418d8e0eeedc6b15
nikhilmindfreak <nikhilbabayolla@gmail.com>
2024-07-15 19:20:24 -0400

    Initial commit

--- install-docker.sh
+++ install-docker.sh
@@ -11,3 +11,11 @@ systemctl start docker
 systemctl enable docker
 usermod -aG docker ec2-user
 echo -e "$R Logout and Login again $N"
+
+# for tempory sdisk space increase for our docker 
+lsblk
+sudo growpart /dev/nvme0n1 4
+sudo lvextend -l +50%FREE /dev/RootVG/rootVol
+sudo lvextend -l +50%FREE /dev/RootVG/varVol
+sudo xfs_growfs /
+sudo xfs_growfs /var
